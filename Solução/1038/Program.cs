aaec3ab8e79506bd50a15e0327ae3d220d954bc2
Thiago Anderson <148602158+ThiagoAnderson@users.noreply.github.com>
2024-10-01 18:02:35 -0300

    Solução 1038
    Solução Exercicio 1038.

---
+++ Solução/1038/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] Pedido = Console.ReadLine().Split(' ');
+var codigo = Convert.ToInt32(Pedido[0]);
+var quantidade = Convert.ToInt32(Pedido[1]);
+ValorTotal(codigo, quantidade);
+
+static void ValorTotal( int codigo , int quantidade)
+{
+    switch (codigo)
+    {
+        case 1:
+            Console.WriteLine($"Total:R$ {4.00 * quantidade:F2}");
+                break;
+        case 2:
+            Console.WriteLine($"Total:R$ {4.50* quantidade:F2}");
+            break;
+        case 3:
+            Console.WriteLine($"Total:R$ {5.00 * quantidade:F2}");
+            break;
+        case 4:
+            Console.WriteLine($"Total:R$ {2.00 * quantidade:F2}");
+            break;
+        case 5:
+            Console.WriteLine($"Total:R$ {1.50 * quantidade:F2}");
+            break;
+    }
+}

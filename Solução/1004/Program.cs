74f1376fe5230be8cb708854c08483b717d62c1c
Thiago Anderson <148602158+ThiagoAnderson@users.noreply.github.com>
2024-10-01 13:47:25 -0300

    Solução 1004
    Solução exercicio 1004.

---
+++ Solução/1004/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+
+        var a = int.Parse(Console.ReadLine());
+        var b = int.Parse(Console.ReadLine());
+        var soma = a * b;
+        Console.WriteLine($"PROD = {soma}");
+
+    }
+
+}

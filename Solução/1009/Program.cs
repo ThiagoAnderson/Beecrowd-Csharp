baea8d2703acb436058361c076c007c2aad7e1a8
Thiago Anderson <148602158+ThiagoAnderson@users.noreply.github.com>
2024-10-01 15:28:29 -0300

    Solução 1009
    Solução Exercicio 1009.

---
+++ Solução/1009/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        var nomeFuncionario = Console.ReadLine();
+        var salario = double.Parse(Console.ReadLine());
+        var vendas = double.Parse(Console.ReadLine());
+        var porcentagemSobVendas = 0.15 * vendas;
+        var valorTotal = salario + porcentagemSobVendas;
+        Console.WriteLine($"TOTAL = R$ {valorTotal:F2}");
+
+    }
+
+}

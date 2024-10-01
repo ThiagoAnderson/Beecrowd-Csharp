310c338759117f80d97cde433d9a6aff93453caf
Thiago Anderson <148602158+ThiagoAnderson@users.noreply.github.com>
2024-10-01 15:20:14 -0300

    Solução 1005
    Solução Exercicio 1005.

---
+++ Solução/1005/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        var notaA = float.Parse(Console.ReadLine());
+        var notaB = float.Parse(Console.ReadLine());
+        var somaDasNotas = ((notaA * 3.5) + (notaB * 7.5)) / 11.00;
+        Console.WriteLine($"MEDIA = {somaDasNotas:F5}");
+
+    }
+
+}

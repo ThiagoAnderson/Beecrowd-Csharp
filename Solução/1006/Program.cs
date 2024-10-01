c8d16d19eda43260de2ff2f7b5c1845fbe9c09b4
Thiago Anderson <148602158+ThiagoAnderson@users.noreply.github.com>
2024-10-01 15:21:49 -0300

    Solução 1006
    Solução do Exercicio 1006.

---
+++ Solução/1006/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        var notaA = float.Parse(Console.ReadLine());
+        var notaB = float.Parse(Console.ReadLine());
+        var notaC = float.Parse(Console.ReadLine());
+        var somaDasNotas = (notaA * 2.0) + (notaB * 3.0) + notaC * 5.0;
+        var mediaDasNotas = somaDasNotas / 10.00;
+        Console.WriteLine($"MEDIA = {mediaDasNotas:F1}");
+
+    }
+
+}

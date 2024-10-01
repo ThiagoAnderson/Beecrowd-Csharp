3077f0e342754774efa8c0177f981ee357e9ee0d
Thiago Anderson <148602158+ThiagoAnderson@users.noreply.github.com>
2024-10-01 13:33:05 -0300

    Solução 1002
    Solução exercicio 1002.

---
+++ Solução/1002/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        var a = int.Parse(Console.ReadLine());
+        var b = int.Parse(Console.ReadLine());
+        int soma = a + b;
+        Console.WriteLine($"X = {soma}");
+
+    }
+
+}

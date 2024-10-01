43d3af6c69ad25a0a15081c0b9e660ba446d9027
Thiago Anderson <148602158+ThiagoAnderson@users.noreply.github.com>
2024-10-01 15:26:32 -0300

    Solução 1008
    Solução Exercicio 1008.

---
+++ Solução/1008/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        var numFuncionario = int.Parse(Console.ReadLine());
+        var numHorasTrabalhadas = int.Parse(Console.ReadLine());
+        var valorPorHora = double.Parse(Console.ReadLine());
+        var salario = numHorasTrabalhadas * valorPorHora;
+        Console.WriteLine($"NUMBER = {numFuncionario}");
+        Console.WriteLine($"SALARY = U$ {salario:F2}");
+
+    }
+
+}

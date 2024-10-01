a0e1e8dd6b2bd39c3ea6db697296a1069b6d4c9b
Thiago Anderson <148602158+ThiagoAnderson@users.noreply.github.com>
2024-10-01 15:23:12 -0300

    Solução 1007
    Solução Exercicio 1007.

---
+++ Solução/1007/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        var a = int.Parse(Console.ReadLine());
+        var b = int.Parse(Console.ReadLine());
+        var c = int.Parse(Console.ReadLine());
+        var d = int.Parse(Console.ReadLine());
+        var diferença = (a * b) - (c * d);
+        Console.WriteLine($"DIFERENCA = {diferença}");
+
+    }
+
+}

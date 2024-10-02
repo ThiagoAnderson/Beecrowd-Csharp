747b4def22f3d0262b7b7185e71c9eaacffb7e48
Thiago Anderson <148602158+ThiagoAnderson@users.noreply.github.com>
2024-10-02 14:12:54 -0300

    Solução 1042
    Solução Exercicio 1042.

---
+++ Solução/1042/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] ValoresIniciais = Console.ReadLine().Split(' ');
+        OrdenarValores(ValoresIniciais);
+    }
+    static void OrdenarValores(string[] ValoresIniciais)
+    {
+        var v0 = Convert.ToInt32(ValoresIniciais[0]);
+        var v1 = Convert.ToInt32(ValoresIniciais[1]);
+        var v2 = Convert.ToInt32(ValoresIniciais[2]);
+
+        int[] ValoresOriginais = { v0, v1, v2 };
+        int[] Valores = { v0, v1, v2 };
+
+        Array.Sort(Valores);
+
+        foreach (int valor in Valores)
+        {
+            Console.WriteLine(valor);
+        }
+
+        Console.WriteLine("");
+
+        foreach (int valor in ValoresOriginais)
+        {
+            Console.WriteLine(valor);
+        }
+    }
+}

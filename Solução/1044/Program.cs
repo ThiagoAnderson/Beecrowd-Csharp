e13db925a328fbfbe93d2261b63ad53cadc32e37
Thiago Anderson <148602158+ThiagoAnderson@users.noreply.github.com>
2024-10-03 12:49:15 -0300

    Solução 1044
    Solução Exercicio 1044.

---
+++ Solução/1044/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string[] Numeros = Console.ReadLine().Split(' ');
+CalcularMultiplos(Numeros);
+static void CalcularMultiplos(string[] num)
+{
+    var x = Convert.ToInt32(num[0]);
+    var y = Convert.ToInt32(num[1]);
+    
+
+    if( Math.Max(x,y) % Math.Min(x,y) == 0)
+    {
+        Console.WriteLine("Sao Multiplos");
+    }
+    else
+    {
+        Console.WriteLine("Nao sao Multiplos");
+    }
+}

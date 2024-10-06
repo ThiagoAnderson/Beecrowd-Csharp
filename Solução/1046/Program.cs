7d3c5a7015642bcd4c613a43fc83404278dc383f
Thiago Anderson <148602158+ThiagoAnderson@users.noreply.github.com>
2024-10-05 21:30:43 -0300

    Solução 1046
    Solução Exercicio 1046.

---
+++ Solução/1046/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] Horarios = Console.ReadLine().Split(' ');
+        CalcularTempo(Horarios);
+
+
+    }
+    static void CalcularTempo(string[] Horarios)
+    {
+        var hrInicio = Convert.ToInt32(Horarios[0]);
+        var hrFim = Convert.ToInt32(Horarios[1]);
+        var duracao = 0;
+
+        if (hrInicio >= hrFim)
+        {
+            duracao = (24 + hrFim) - hrInicio;
+            Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");
+        }
+        else
+        {
+            duracao = hrFim - hrInicio;
+            Console.WriteLine($"O JOGO DUROU {duracao} HORA(S)");
+        }
+
+    }
+
+}

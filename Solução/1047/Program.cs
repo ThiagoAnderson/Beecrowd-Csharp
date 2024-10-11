f7bfbe4e8f71f1bedbec4c624c3423199002bcd4
Thiago Anderson <148602158+ThiagoAnderson@users.noreply.github.com>
2024-10-11 11:49:27 -0300

    Solução 1047
    Solução Exercicio 1047

---
+++ Solução/1047/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] valores = Console.ReadLine().Split(' ');
+        CalcularTempo(valores);
+
+    }
+    static void CalcularTempo(string[] valores)
+    {
+        int hi = Convert.ToInt32(valores[0]);
+        int mi = Convert.ToInt32(valores[1]);
+        int hf = Convert.ToInt32(valores[2]);
+        int mf = Convert.ToInt32(valores[3]);
+
+        int instanteInicial = hi * 60 + mi;
+        int instanteFinall = hf * 60 + mf;
+        int duracao;
+
+        if (instanteInicial < instanteFinall)
+        {
+            duracao = instanteFinall - instanteInicial;
+        }
+        else
+        {
+            duracao = (24 * 60 - instanteInicial) + instanteFinall;
+        }
+
+        int duracaoHoras = duracao / 60;
+        int duracaoMinutos = duracao % 60;
+
+        Console.WriteLine($"O JOGO DUROU {duracaoHoras} HORA(S) E {duracaoMinutos} MINUTO(S)");
+
+    }
+}

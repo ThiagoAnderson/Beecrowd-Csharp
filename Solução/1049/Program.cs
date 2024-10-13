0383cecd96171a3a37413eee08bb34dae4d1a75f
Thiago Anderson <148602158+ThiagoAnderson@users.noreply.github.com>
2024-10-13 13:41:12 -0300

    Solução Exercicio 1049
    Exercicio 1049

---
+++ Solução/1049/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        var tipoPrincipal = Console.ReadLine().ToLower();
+        var tipoSecundario = Console.ReadLine().ToLower();
+        var tipoTerciario = Console.ReadLine().ToLower();
+        definirAnimal(tipoPrincipal, tipoSecundario, tipoTerciario);
+
+    }
+    static void definirAnimal(string primario, string secundario, string terciario)
+    {
+        if (primario == "vertebrado")
+        {
+            if (secundario == "ave")
+            {
+                if (terciario == "carnivoro")
+                {
+                    Console.WriteLine("aguia");
+                }
+                else if (terciario == "onivoro")
+                {
+                    Console.WriteLine("pomba");
+                }
+            }
+            else if (secundario == "mamifero")
+            {
+                if (terciario == "onivoro")
+                {
+                    Console.WriteLine("homem");
+                }
+                else if (terciario == "herbivoro")
+                {
+                    Console.WriteLine("vaca");
+                }
+            }
+        }
+        else if (primario == "invertebrado")
+        {
+            if (secundario == "inseto")
+            {
+                if (terciario == "hematofago")
+                {
+                    Console.WriteLine("pulga");
+                }
+                else if (terciario == "herbivoro")
+                {
+                    Console.WriteLine("lagarta");
+                }
+            }
+            else if (secundario == "anelideo")
+            {
+                if (terciario == "hematofago")
+                {
+                    Console.WriteLine("sanguessuga");
+                }
+                else if (terciario == "onivoro")
+                {
+                    Console.WriteLine("minhoca");
+                }
+            }
+        }
+    }
+
+
+}

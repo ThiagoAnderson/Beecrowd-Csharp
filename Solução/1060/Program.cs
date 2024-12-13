9e59560bac4cef62920cd2e2e15c8ff3d9799717
Thiago Anderson <148602158+ThiagoAnderson@users.noreply.github.com>
2024-12-13 10:40:05 -0300

    Solução Exercicio 1060

---
+++ Solução/1060/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        ContarInteiros();
+    }
+
+    static void ContarInteiros()
+    {
+        List<float> numeros = new List<float>();
+
+        for (int i = 0; i < 6; i++)
+        {
+            float num = float.Parse(Console.ReadLine());
+            if (num > 0)
+            {
+                numeros.Add(num);
+            }
+        }
+        Console.WriteLine($"{numeros.Count} valores positivos");
+    }
+}
